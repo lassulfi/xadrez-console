b7b259f29e12e7c2f323c14a0f908ace9fb373bb
Luis Daniel Assulfi <lassulfi@gmail.com>
2018-08-03 12:46:16 -0300

    Projeto criado

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
